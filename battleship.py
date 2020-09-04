714325acefb2f2a888f2c762ed063322b658a91d
Liam Connolly <liam.connolly@ku.edu>
2020-09-04 13:50:18 -0500

    Added comments of where the classes will go Also started on the GameFlow class

--- battleship.py
+++ battleship.py
@@ -1,7 +1,23 @@
 import pygame
 import numpy
 import random
+from abc import ABC
 
 
+# controls the overall flow of the game
+class GameFlow:
+    def play(self):
+        print("Welcome to battleship")
 
-print("Welcome to battleship")
+# controls the display
+# class Display:
+
+# creates ship objects that store data about the ships
+# class Ship:
+
+
+# class Board(ABC):
+
+# class Player(Board):
+
+# class Computer(Board):
