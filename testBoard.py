4016d89da6f126e2989418aeb8cbf3c7f715d099
Carter Harrod <carter.harrod283@gmail.com>
2020-09-05 20:19:06 -0500

    cleaned up testBoard

--- testBoard.py
+++ testBoard.py
@@ -7,92 +7,21 @@ from Tile import Tile
 board = Board()
 fleet = [Ship("carrier", 5), Ship("battleship", 4), Ship("destroyer", 3), Ship("gunboat", 2), Ship("dinghy", 1)]
 
-
-try:
-    board.placeShip("up", fleet[1], -1, 0)
-    print("Exception X < 0 Test: Failed")
-except:
-    print("Exception X < 0 Test: Passed")
-
-try:
-    board.placeShip("up", fleet[1], 9, 0)
-    print("Exception X > 8 Test: Failed")
-except:
-    print("Exception X > 8 Test: Passed")
-
-
-try:
-    board.placeShip("up", fleet[1], 0, -1)
-    print("Exception Y < 0 Test: Failed")
-except:
-    print("Exception Y < 0 Test: Passed")
-
-try:
-    board.placeShip("up", fleet[1], 0, 9)
-    print("Exception Y > 8 Test: Failed")
-except:
-    print("Exception Y > 8 Test: Passed")
-
-if not (board.placeShip("up", fleet[1], 1, 1)):
-    print("Invalid Placement up: Passed")
-else:
-    print("Invalid Placement up: Failed")
-
-if not (board.placeShip("left", fleet[1], 1, 1)):
-    print("Invalid Placement left: Passed")
-else:
-    print("Invalid Placement left: Failed")
-
-if not (board.placeShip("down", fleet[1], 7, 7)):
-    print("Invalid Placement down: Passed")
-else:
-    print("Invalid Placement down: Failed")
-
-if not (board.placeShip("right", fleet[1], 7, 7)):
-    print("Invalid Placement right: Passed")
-else:
-    print("Invalid Placement right: Failed")
-
-
-
-
 placement = True
-if (board.placeShip("right", fleet[1], 2, 3)):
-    for y in range(fleet[1].getHealth()):
-        if (board.getTile(1, 1 + y).getTileItem() != fleet[1].getName()):
-            placement = False
-else:
-    placement = False
-if placement:
-    print("Placement Test Down: Passed")
-else:
-    print("Placement Test Down: Failed")
-
-print(board.getTile(1,4).getTileItem())
-
-
-
-
-if not (board.placeShip("up", fleet[2], 3, 4)):
-    print("Placement Test Collision: Passed")
-else:
-    print("Placement Test Collision: Failed")
-
-
-
+print(board.placeShip("right", fleet[2], 0, 1))
 
 for y in range(9):
-    printCheck = ""
+    printRow = ""
     for x in range(9):
-        printCheck += board.getTile(x, y).getTileItem()[0] + " "
-    print(printCheck)
+        printRow += board.getTile(x, y).getTileItem()[0] + " "
+    print(printRow)
 print("")
 
 for y in range(9):
-    printCheck = ""
+    printRow = ""
     for x in range(9):
-        printCheck += str(board.getTile(x, y).getTileAttacked())[0] + " "
-    print(printCheck)
+        printRow += str(board.getTile(x, y).getTileAttacked())[0] + " "
+    print(printRow)
 print("")
 
 x = 2
@@ -107,10 +36,10 @@ else:
 
 
 for y in range(9):
-    printCheck = ""
+    printRow = ""
     for x in range(9):
-        printCheck += str(board.getTile(x, y).getTileAttacked())[0] + " "
-    print(printCheck)
+        printRow += str(board.getTile(x, y).getTileAttacked())[0] + " "
+    print(printRow)
 
 for boat in fleet:
     print(boat.getName() + " " + str(boat.getHealth()))
