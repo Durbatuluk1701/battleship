6550414d19e9a78f2c17ac8c3545eaa110121386
Liam Connolly <liam.connolly@ku.edu>
2020-09-06 20:03:16 -0500

    Finished the beginning initialization

--- battleship.py
+++ battleship.py
@@ -16,3 +16,10 @@ while gameBeingPlayed:
     d = Display() #initializes the display
     game = GameFlow(d) #initializes the GameFlow
     game.play() #calls GameFlow's Play function
+
+    playAgain = input("Would you like to play again? y/n: ")
+    while playAgain is not 'y' or 'n':
+        playAgain = input("Error! Must be a valid response! y/n: ")
+    if playAgain == 'n':
+        print("Thanks for playing!")
+        break
