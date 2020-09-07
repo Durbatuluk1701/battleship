14da6c56f55d242718ebd058d114e58ded517344
Turner Graham <t467g332@ku.edu>
2020-09-07 11:32:31 -0500

    Definition of shipPlace method in Computer class

--- Computer.py
+++ Computer.py
@@ -1,17 +1,27 @@
 class computer:
     def __init__(self)
         self.board = board
-    def shipPlace(self, tileItem) # 
-        self.board.placeShip
+    def shipPlace(self, ship) # 
         # generate random x,y coordinates
         # check if it can place the ship without going out of bounds
         # if it can, it will place the ship
         # if not, generate another set of coordinates (not the same) and try again until it can place ship
+        shipPlaced = False
+        while (shipPlaced == False)
+            x = random.randint(0,9)
+            y = random.randint(0,9)
+            if (self.board.placeShip("up", ship, x, y))
+                shipPlaced == True
+            else if (self.board.placeShip("down", ship, x, y))
+                shipPlaced == True
+            else if (self.board.placeShip("left", ship, x, y))
+                shipPlaced == True
+            else if (self.board.placeShip("right", ship, x, y))
+                shipPlaced == True
+            else:
+                pass
     def shipGuess(self)
         # check if it has hit a ship before
         # if it has, check the adjacent tiles until it fully sinks the ship
         # if it hasn't hit something before, generate random x,y coordinates
         # attack those coordinates
-    def randGen(self)
-        # return 2 integers for x,y coordinates
-        # random.randint(a,b)
