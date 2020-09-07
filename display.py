07e0d4d24ff1dd13e20b2cf6e381510d3b667682
Jarrett <j287z337@ku.edu>
2020-09-07 18:59:06 -0500

    I am trying to rebuild this from scratch, my main issue is creating a correct looking board, I am able to create the window/set the dimensions, this currently just opens a fully black screen

--- display.py
+++ display.py
@@ -3,53 +3,52 @@ import sys
 
 class Display:
 
-    ocean = (52,196,206) #colors and dimensions
-    black = (0,0,0)
-    red = (255,14,14) # this is a hit
-    brown = (94,45,8) # this is a ship
-    width = 20
-    height = 20
-    margin = 5
+    colors = { #initalize all colors that will be used
+    "water" : pygame.color.Color(21,72,161),
+    "ship" : pygame.color.Color(102,51,0),
+    "hit" : pygame.color.Color(255,0,0),
+    "miss" : pygame.color.Color(235,214,233),
+    "background" : pygame.color.Color(0,0,0),
+    "text" : pygame.color.Color(255,255,255)
+    }
 
+    def __init__ (self, board_size = 10, cell_size = 20, margin = 10): #constructor of the display, dimensions
+        self.board_size = board_size
+        self.cell_size = cell_size
+        self.margin = margin
+        pygame.init()
+        pygame.font.init()
+        self.font = pygame.font.SysFont("ComicSans",15) # sets font
+        screen_width = self.cell_size * board_size + 2 * margin
+        screen_height = 2 * self.cell_size *board_size + 3 * margin
+        self.screen = pygame.display.set_mode([screen_width, screen_height])
+        pygame.display.set_caption("Battleship!") #name of window
+        clock = pygame.time.Clock()
+        return screen, clock, display
 
-    grid = []
-    for row in range(21):
-        grid.append([])
-        for column in range(19):
-            grid[row].append(0)
-
-
-    pygame.init()
-    window_size = [510,600] # dimensions of the window
-    screen = pygame.display.set_mode(window_size) #puts game in windowed mode
-    pygame.display.set_caption("Battleship!") # name of window
-    done = False # loop condition
-    clock = pygame.time.Clock()
-
-    while not done:
+    def input (self,running = True): # user interaction with the display
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            elif event.type == pygame.MOUSEBUTTONDOWN: #when user mouse buttons down
-                pos = pygame.mouse.get_pos()
-                column = pos[0] // (width + margin)
-                row = pos[1] // (height + margin)
-                grid[row][column] = 1 #this indicates a pressed/hit square
+            if event.type == pygame.QUIT: #user wnats to quit the game
+                Display.close()
+            elif event.type == pygame.MOUSEBUTTONDOWN: # when user mouse buttons down
+                x, y = pygame.mouse.get_pos()
+                y = y % (self.board_size * self.cell_size + self.margin)
+                x = (x - self.margin) // self.cell_size
+                y = (y - self.margin) // self.cell_size
+                if x in range(self.board_size) and y in range(self.board_size):
+                    return x, y
+        return None, None
 
 
-        screen.fill(black) #background color
-        for row in range(20): #player grid being drawn
-            for column in range(9):
-                color = ocean
-                if grid[row][column] == 1:
-                    color = red
-                pygame.draw.rect(screen,color,[(margin + width) * column + margin,(margin + height) * row +margin, width, height] )
-        if grid [row][9]:
-            color = black
+pygame.init()
+pygame.display.set_caption("Battleship!") # name of window
+window_size = [600,600]
+screen = pygame.display.set_mode(window_size)
+running = True
+while running:
+    pygame.display.flip() # updates the visuals on the board
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False # the user wants to quit the game
 
-
-
-        clock.tick(30) #frame rate
-        pygame.display.flip() # updates the board
-
-    pygame.quit()
+pygame.quit()
