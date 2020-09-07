0a35ae1983f8625f3e2180fe7e510b6be68c99a2
Turner Graham <t467g332@ku.edu>
2020-09-07 12:18:01 -0500

    Fixing syntax errors in Computer class

--- Computer.py
+++ Computer.py
@@ -1,27 +1,27 @@
-class computer:
-    def __init__(self)
-        self.board = board
-    def shipPlace(self, ship) # 
+class Computer:
+    def __init__(self):
+        self.board = Board
+    def shipPlace(self, ship): 
         # generate random x,y coordinates
         # check if it can place the ship without going out of bounds
         # if it can, it will place the ship
         # if not, generate another set of coordinates (not the same) and try again until it can place ship
         shipPlaced = False
-        while (shipPlaced == False)
+        while shipPlaced == False:
             x = random.randint(0,9)
             y = random.randint(0,9)
-            if (self.board.placeShip("up", ship, x, y))
+            if (self.board.placeShip("up", ship, x, y)):
                 shipPlaced == True
-            else if (self.board.placeShip("down", ship, x, y))
+            elif (self.board.placeShip("down", ship, x, y)):
                 shipPlaced == True
-            else if (self.board.placeShip("left", ship, x, y))
+            elif (self.board.placeShip("left", ship, x, y)):
                 shipPlaced == True
-            else if (self.board.placeShip("right", ship, x, y))
+            elif (self.board.placeShip("right", ship, x, y)):
                 shipPlaced == True
             else:
                 pass
-    def shipGuess(self)
+    def shipGuess(self):
         # check if it has hit a ship before
         # if it has, check the adjacent tiles until it fully sinks the ship
         # if it hasn't hit something before, generate random x,y coordinates
-        # attack those coordinates
+        # attack those coords
