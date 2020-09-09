63d570dc6695cd5a2b9476dfb2fcc3a1e0b15761
Jarrett <j287z337@ku.edu>
2020-09-08 22:39:11 -0500

    Some visuals are working from the visual definition, I still cant access the colors within the definition and I am not sure why, Still need to properly format the grid

--- display.py
+++ display.py
@@ -3,26 +3,26 @@ import sys
 
 class Display:
 
-    colors = { #initalize all colors that will be used
-    "water" : pygame.color.Color(21,72,161),
-    "ship" : pygame.color.Color(102,51,0),
-    "hit" : pygame.color.Color(255,0,0),
-    "miss" : pygame.color.Color(235,214,233),
-    "background" : pygame.color.Color(0,0,0),
-    "text" : pygame.color.Color(255,255,255)
-    }
+    water = (21,72,161)
+    ship = (102,51,0)
+    hit = (255,0,0)
+    miss = (235,214,233)
+    background = (0,0,0)
+    text = (255,255,255)
 
-    def __init__ (self, board_size = 20, cell_size = 20, margin = 10): #constructor of the display, dimensions
+    def __init__ (self, board_size = 20, cell_size = 20, margin = 15): #constructor of the display, dimensions
         self.board_size = board_size
         self.cell_size = cell_size
         self.margin = margin
-        #self.fill = (255,0,0)
         pygame.init()
         pygame.font.init()
         self.font = pygame.font.SysFont("ComicSans",15) # sets font
         screen_width = self.cell_size * board_size + 2 * margin # sets width
+        self.screen_width = screen_width
         screen_height = 2 * self.cell_size *board_size + 3 * margin #sets heigth
-        self.screen = pygame.display.set_mode([screen_width, screen_height])
+        self.screen_height = screen_height
+        screen = pygame.display.set_mode([screen_width, screen_height])
+        self.screen = screen
         pygame.display.set_caption("Battleship!") #name of window
         clock = pygame.time.Clock()
 
@@ -39,7 +39,22 @@ class Display:
                     return x, y
         return None, None
 
+    def visual(self):
+        water = (21,72,161)
+        grid = []
+        for row in range(21):
+            grid.append([])
+            for column in range(19):
+                grid[row].append(0)
+        for row in range(20):
+            for column in range(9):
+                color = water
+                if grid[row][column] == 1:
+                    color = red
+                pygame.draw.rect(mygame.screen,color,[(mygame.margin + mygame.screen_width) * column + mygame.margin, (mygame.margin + mygame.screen_height) * row + mygame.margin, mygame.screen_width, mygame.screen_height])
+
 mygame = Display()
+mygame.visual()
 running = True
 while running:
     pygame.display.flip() # updates the visuals on the board
