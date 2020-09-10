bef3d29a8d96d32f0ab072346293c47e1d316884
Jarrett <j287z337@ku.edu>
2020-09-09 22:03:23 -0500

    Implemented two grids to the display, still need to be able to adjust colors based on mouse click, and label the sides

--- display.py
+++ display.py
@@ -10,19 +10,20 @@ class Display:
     background = (0,0,0)
     text = (255,255,255)
 
-    def __init__ (self, board_size = 20, cell_size = 20, margin = 15): #constructor of the display, dimensions
+    def __init__ (self, board_size = 25, cell_size = 20, margin = 15): #constructor of the display, dimensions
         self.board_size = board_size
         self.cell_size = cell_size
         self.margin = margin
         pygame.init()
         pygame.font.init()
         self.font = pygame.font.SysFont("ComicSans",15) # sets font
-        screen_width = self.cell_size * board_size + 2 * margin # sets width
+        screen_width = 470 #self.cell_size * board_size + 2 * margin # sets width
         self.screen_width = screen_width
-        screen_height = 2 * self.cell_size *board_size + 3 * margin #sets heigth
+        screen_height = 900 #2 * self.cell_size *board_size + 3 * margin #sets heigth
         self.screen_height = screen_height
         screen = pygame.display.set_mode([screen_width, screen_height])
         self.screen = screen
+        self.screen.fill((0,0,0))
         pygame.display.set_caption("Battleship!") #name of window
         clock = pygame.time.Clock()
 
@@ -40,18 +41,18 @@ class Display:
         return None, None
 
     def visual(self):
-        water = (21,72,161)
-        grid = []
-        for row in range(21):
-            grid.append([])
-            for column in range(19):
-                grid[row].append(0)
-        for row in range(20):
-            for column in range(9):
-                color = water
-                if grid[row][column] == 1:
-                    color = red
-                pygame.draw.rect(mygame.screen,color,[(mygame.margin + mygame.screen_width) * column + mygame.margin, (mygame.margin + mygame.screen_height) * row + mygame.margin, mygame.screen_width, mygame.screen_height])
+        white = (255,255,255)
+        blockSize = 40
+        for y in range(9):
+            for x in range(9):
+                square = pygame.Rect(50 + x*blockSize, y*blockSize + 40, blockSize, blockSize)
+                pygame.draw.rect(mygame.screen, white, square, 1)
+
+        buffer = self.margin / 30 + self.board_size * self.cell_size # 30 + 25 * 15
+        for y in range(9):
+            for x in range(9):
+                square = pygame.Rect(50 + x*blockSize, y*blockSize + buffer, blockSize, blockSize)
+                pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 1)
 
 mygame = Display()
 mygame.visual()
