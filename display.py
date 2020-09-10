02339a9a46269a2d07dc0e4c62b233d6ef977743
Jarrett <j287z337@ku.edu>
2020-09-09 23:48:25 -0500

    WIP, grid is working and is spaced properly, still need to figure out how to make it clickable (which is the grid code I am working on), in addition to that still need labels

--- display.py
+++ display.py
@@ -26,33 +26,45 @@ class Display:
         self.screen.fill((0,0,0))
         pygame.display.set_caption("Battleship!") #name of window
         clock = pygame.time.Clock()
+        grid = []
+        for row in range(21):
+            grid.append([])
+            for column in range(19):
+                grid[row].append(0)
+        self.grid = grid
 
     def input (self,running = True): # user interaction with the display
         for event in pygame.event.get():
             if event.type == pygame.QUIT: #user wnats to quit the game
                 Display.close()
             elif event.type == pygame.MOUSEBUTTONDOWN: # when user mouse buttons down
-                x, y = pygame.mouse.get_pos()
-                y = y % (self.board_size * self.cell_size + self.margin)
-                x = (x - self.margin) // self.cell_size
-                y = (y - self.margin) // self.cell_size
-                if x in range(self.board_size) and y in range(self.board_size):
-                    return x, y
-        return None, None
+                pos = pygame.mouse.get_pos()
+                column = pos[0] // (width + margin)
+                row = pos[1] // (height + margin)
+                self.grid[row][column] = 1 #this indicates a pressed/hit square
 
     def visual(self):
         white = (255,255,255)
+        red = (255,14,14)
         blockSize = 40
         for y in range(9):
-            for x in range(9):
+            for x in range(9): # this is the top grid
                 square = pygame.Rect(50 + x*blockSize, y*blockSize + 40, blockSize, blockSize)
                 pygame.draw.rect(mygame.screen, white, square, 1)
 
         buffer = self.margin / 30 + self.board_size * self.cell_size # 30 + 25 * 15
-        for y in range(9):
+        for y in range(9): # this is the bottom grid
             for x in range(9):
                 square = pygame.Rect(50 + x*blockSize, y*blockSize + buffer, blockSize, blockSize)
                 pygame.draw.rect(self.screen, (255, 255, 255, 255), square, 1)
+        #for row in range(20): #player grid being drawn
+            #for column in range(9):
+                #if self.grid[row][column] == 1:
+                    #color = red
+            #    pygame.draw.rect(mygame.screen,color,[(self.margin + self.screen_width) * column + self.margin,(self.margin + self.screen_height) * row + self.margin, self.screen_width, self.screen_height] )
+
+
+
 
 mygame = Display()
 mygame.visual()
