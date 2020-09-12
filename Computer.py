95385902302c454a76a61805fd5fd4b3417f79ca
Turner Graham <t467g332@ku.edu>
2020-09-12 17:03:35 -0500

    Changes made to documentation on Computer class

--- Computer.py
+++ Computer.py
@@ -4,31 +4,31 @@ from Ship import Ship
 from Tile import Tile
 class Computer:
     def __init__(self):
-    """
-    Default Constructor
-    Parameters: n/a
-    Returns: n/a
-    Preconditions: n/a
-    Postconditions: A Computer object is created with a default Board object within it.
-    """
+        """
+        Default Constructor
+        Parameters: n/a
+        Returns: n/a
+        Preconditions: n/a
+        Postconditions: A Computer object is created with a default Board object within it.
+        """
         self.__board__ = Board()
     def getBoard(self):
-    """
-    Get Board Method
-    Parameters: n/a
-    Returns: The Board object of the Computer.
-    Preconditions: The Board of the Computer is created.
-    Postconditions: n/a
-    """
+        """
+        Get Board Method
+        Parameters: n/a
+        Returns: The Board object of the Computer.
+        Preconditions: The Board of the Computer is created.
+        Postconditions: n/a
+        """
         return(self.__board__)
     def shipPlace(self, ship): 
-    """
-    Ship Place Method
-    Parameters: A Ship object is passed in to the method.
-    Returns: n/a
-    Preconditions: A Board object is created and valid Ship object is passed in.
-    Postconditions: The Computer places the Ship that is passed in onto its Board.
-    """
+        """
+        Ship Place Method
+        Parameters: A Ship object is passed in to the method.
+        Returns: n/a
+        Preconditions: A Board object is created and valid Ship object is passed in.
+        Postconditions: The Computer places the Ship that is passed in onto its Board.
+        """
         # generate random x,y coordinates
         # check if it can place the ship without going out of bounds
         # if it can, it will place the ship
@@ -48,22 +48,22 @@ class Computer:
             else:
                 pass
     def attackTile(self, xCoord, yCoord):
-    """
-    Attack Tile Method
-    Parameters: An x coordinate and a y coordinate (both ints) for the Board.
-    Returns: True if the Tile has been attacked before, False otherwise.
-    Preconditions: A Board object is created and valid coordinates are passed in.
-    Postconditions: The Tile of the Board at the specified coordinates is attacked, if possible.
-    """
+        """
+        Attack Tile Method
+        Parameters: An x coordinate and a y coordinate (both ints) for the Board.
+        Returns: True if the Tile has been attacked before, False otherwise.
+        Preconditions: A Board object is created and valid coordinates are passed in.
+        Postconditions: The Tile of the Board at the specified coordinates is attacked, if possible.
+        """
         return(self.__board__.attackTile(xCoord, yCoord))
     def shipGuess(self):
-    """
-    Ship Guess Method
-    Parameters: n/a
-    Returns: n/a
-    Preconditions: A Board object is created.
-    Postconditions: The Computer attacks the Tile at the randomly generated coordinates.
-    """
+        """
+        Ship Guess Method
+        Parameters: n/a
+        Returns: n/a
+        Preconditions: A Board object is created.
+        Postconditions: The Computer attacks the Tile at the randomly generated coordinates.
+        """
         # generate random x,y coordinates
         # attack those coords, if possible
         openTile = False
