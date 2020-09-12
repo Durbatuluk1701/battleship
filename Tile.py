6b9e2f0651f54aaff0963e4f7c6daa04ed843a5b
Dawson Kabler <dhkabler@ku.edu>
2020-09-12 16:57:09 -0500

    Documentation to Tile class

--- Tile.py
+++ Tile.py
@@ -1,45 +1,56 @@
-'''Class: Tile
-    The Tile class is used to hold information and interact with a grid space in battleship.'''
+
 class Tile:
 
-    '''-Default Constructor-
+    
+    def __init__(self): 
+        '''
         Parameters: n/a
         Return: n/a
         Preconditions: Must create a Tile object.
         Postconditions: Sets the grid tile to "water" and that it hasn't been attacked yet.
         '''
-    def __init__(self): # initializes the tile object to be water and not attacked by default
+
         self.tileItem = "water" # tileItem is a string
         self.attacked = False # attacked is a bool
 
-    '''Function: getTile()
+   
+    def getTileItem(self):
+        '''
         Parameters: n/a
         Return: returns tileItem
         Preconditions: Must create a Tile object.
-        Postconditions: returns what is in the tile at a specific grid space.'''
-    def getTileItem(self): # returns the type of tile
+        Postconditions: returns what is in the tile at a specific grid space.
+        '''
+
         return self.tileItem
 
-    '''Function: getTileAttacked()
+    def getTileAttacked(self):
+        '''
         Parameters: n/a
         Return: returns attacked.
         Preconditions: Must create a Tile object.
-        Postconditions: returns the bool of if the grid space has already been attacked.'''
-    def getTileAttacked(self): # returns the status of the tile, regarding whether or not it's been attacked
+        Postconditions: returns the bool of if the grid space has already been attacked.
+        '''
+        
         return self.attacked
 
-    '''Function: setTileAttacked()
+    def setTileAttacked(self):
+        '''
         Parameters: n/a
         Return: n/a
         Preconditions: Must create a Tile object.
-        Postconditions: Sets the tile to True showing it has been attacked.'''
-    def setTileAttacked(self): # sets the status of the tile to attacked
+        Postconditions: Sets the tile to True showing it has been attacked.
+        '''
+        
         self.attacked = True
 
-    '''Function: setTileItem()
+    
+    def setTileItem(self, tileItem):
+        '''
         Parameters: tileItem
         Return: n/a
         Preconditions: Must create a Tile object.
-        Postconditions: Sets the tile to the parameter of tileItem.'''
-    def setTileItem(self, tileItem): # sets the type of tile
+        Postconditions: Sets the tile to the parameter of tileItem.
+        '''
+        
         self.tileItem = tileItem
