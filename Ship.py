31944c588f15bb81a57e333896b0576f1b61d942
Dawson Kabler <dhkabler@ku.edu>
2020-09-12 16:54:23 -0500

    Documentation of Ship class

--- Ship.py
+++ Ship.py
@@ -1,58 +1,66 @@
-
-'''Class: Ship
-    The Ship class is used to hold and do any actions that have to deal with ships in the battleship game.'''
-
-'''Private Member Varibles:
-    __shipName__ : holds name for ship object.
-    __health__ : the health of a ship object.
-    __shipLength__ : how long a ship object is.
-    __shipSpots__ : holds the information on where a ship object is on the board.'''
 class Ship:
+    '''
+    Class: Ship
+    The Ship class is used to hold and do any actions that have to deal with ships in the battleship game.
+    '''
+
     __shipName__ = ""
     __health__ = 0
     __shipLength__ = 0
     __shipSpots__ = []
-    
-    '''Default Constructor:
+
+    def __init__(self, name, health):
+        '''
         Parameters : n/a
         Return: n/a
         Preconditions: Need to create a ship object.
-        Postconditions: Sets the name and the health of a ship object.'''
-    def __init__(self, name, health): # assigns health and name in default constructor
+        Postconditions: Sets the name and the health of a ship object.
+        '''
+        
         self.__shipName__ = name
         self.__health__= health
 
-    '''Function : getName()
+
+    
+    def getName(self):
+        '''
         Parameters : n/a
         Return: The private member varible __shipname__.
         Preconditions: n/a
-        Postconditions: Gives the name of a ship object.'''
-    def getName(self):
+        Postconditions: Gives the name of a ship object.
+        '''
+
         return self.__shipName__
 
-        '''Function : getHealth()
+    def getHealth(self):
+        '''
         Parameters : n/a
         Return: The private member varible __health__.
         Preconditions: n/a
-        Postconditions: Gives current health of a ship object.'''
-    def getHealth(self):
+        Postconditions: Gives current health of a ship object.
+        '''
+
         return self.__health__
 
-    '''Function : damageShip()
+    def damageShip(self):
+        '''
         Parameters : n/a
         Return: n/a
         Preconditions: To be called by ship object.
-        Postconditions: Does one damage to a ship that has at least one health.'''
-    def damageShip(self): # damages ship health by 1
+        Postconditions: Does one damage to a ship that has at least one health.
+        '''
+
         if self.__health__ > 0:
             self.__health__ = self.__health__ - 1
 
-    '''Function : isDead()
+    def isDead(self):
+        '''
         Parameters : n/a
         Return: returns true if the ship object is dead, and false if it is still alive.
         Preconditions: Needs to be called by ship object.
-        Postconditions: Tells us if the ship we are calling is dead or alive.'''
-    def isDead(self): # if health is 0, ship is dead
+        Postconditions: Tells us if the ship we are calling is dead or alive.
+        '''
+
         if self.__health__ == 0:
             return True
         return False
